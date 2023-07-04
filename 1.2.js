d759fb062fb06afeef2c47a5e5388ac1c4d45127
Abu.khater90 <abu.khater90@gmail.com>
2023-07-04 20:03:23 +0300

    copied from pdf, and want to fix

---
+++ 1.2.js
@@ -0,0 +1,61 @@
+// 1. Which of the following is/are strings?
+// c) '4'
+// 2. Which of the following is/are numbers?
+// a) 4
+// b) 4.0
+// d) -4
+// &
+// C
+// ⑬
+// 3. Which of the following is/are booleans?
+// a) true
+// b) false
+// c) "true"
+// 4. What is the result of 80 + 71.2?
+// a) 151.2
+// b) 151
+// c) 8071.2
+// 5. What is the result of "80" + 71.2?
+// a) 151.2
+// b) 151
+// c) "8071.2"
+// 6. Does 100 + 30 produce a number or a string?
+// a) number
+// b)string
+// 7. Does "100" + 30 produce a number or a string?
+// a)number
+// b)string
+// */
+// ⑧
+// 0
+// O
+// 8
+// O
+// //------Submit your answers under the
+// question------//
+// //create three different ways to declare variables
+// //declare a variable and reassign a value to it
+// //create a variable and after that assign a string
+// with its value being: He's got it!
+// /*
+// 1. create a variable and assign a value on how
+// much a restaurant bill is.
+// 2. create a variable and assign a value on how
+// much tax they should pay.
+// 3. create a variable that will calculate the bill
+// * tax and its output would be: Your total bill is
+// <total bill> $.
+// */
+// // Round the number 50.6 to its nearest integer
+// //Create a variable that is undefined
+// ①Let far,"Test"cartest: "Test"
+// 1 etTest: "Test
+// letword: "He's gotit!
+// & 1 etbill =
+// 200 ③ let words
+// ③ 1 et tax:0,17 "your total bill
+// is -I
+// 1 et result: "BEbill Taxi $d soul by
+// l
+
+console.log(1+1)
