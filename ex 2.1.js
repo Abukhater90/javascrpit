1de9ea4dd84cb6e1aa69f9c0d5c26eb44adc2af4
Abukhater90 <93696256+Abukhater90@users.noreply.github.com>
2023-07-06 14:18:02 +0300

    Add files via upload

---
+++ ex 2.1.js
@@ -0,0 +1,12 @@
+
+
+function countryInfo (country , population , capitalCity){
+
+    let result = `${country} has ${population} million people and its capital city is ${capitalCity}` 
+
+return result;
+
+}
+
+var france = countryInfo ("jordan" , 20 , "amman" )
+console.log(france);
