175171fb2bcd45ef94a466c602b7708a84920e3b
Abukhater90 <93696256+Abukhater90@users.noreply.github.com>
2023-07-06 15:17:25 +0300

    Add files via upload

---
+++ ex 2.4.js
@@ -0,0 +1,13 @@
+
+function date (dayName ,  month , year){
+
+
+// `Today is Sunday the 31 of January 2021’
+
+
+let result = ` today is ${dayName} the 31 of ${month} of  ${year}`
+console.log(result);
+
+}
+
+date ("friday" , "August" , 2023);
